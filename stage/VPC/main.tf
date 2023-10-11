1d8ed54f1fde53bb64a78c5878c8b2a331a56103
Junghun-lovepower <tjdghks9585@gmail.com>
2023-10-11 15:02:54 +0900

    Remote Backend

--- stage/VPC/main.tf
+++ stage/VPC/main.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+    bucket  = "myterraform-bucket-state-park-j"
+    key     = "stage/app1/terraform.tfstate"
+    region  = "ap-northeast-2"
+    profile = "terraform_user"
+    dynamodb_table = "myTerraform-bucket-lock-park-j"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -12,49 +19,160 @@ provider "aws" {
   profile = "terraform_user"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "myterraform-bucket-state-park-j"
+
+//================================================//
+
+
+/* # Stage VPC
+module "stage_vpc" {
+  source = "github.com/Junghun-lovepower/Terraform_Project_VPC"
+  name   = "stage_vpc"
+  cidr   = local.cidr
+
+  azs              = local.azs
+  public_subnets   = local.public_subnets
+  private_subnets  = local.private_subnets
+  database_subnets = local.database_subnets
+
+  enable_dns_hostnames = "true"
+  enable_dns_support   = "true"
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+
   tags = {
-    Name = "terraform_state"
+    "TerraformManaged" = "true"
   }
-
-  lifecycle {
-    #prevent_destroy = true
-  }
-
-  force_destroy = true
 }
 
-resource "aws_kms_key" "terraform_state_kms" {
-  description             = "terraform_state_kms"
-  deletion_window_in_days = 7
-}
+//================================================//
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_sec" {
-  bucket = aws_s3_bucket.terraform_state.id
 
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.terraform_state_kms.arn
-      sse_algorithm     = "aws:kms"
+# SSH SG
+module "SSH_SG" {
+  source          = "github.com/Junghun-lovepower/Terraform_Project_SG"
+  name            = "SSH_SG"
+  description     = "SSH Port Allow"
+  vpc_id          = module.stage_vpc.vpc_id
+  use_name_prefix = "false"
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = local.ssh_port
+      to_port     = local.ssh_port
+      protocol    = local.tcp_protocol
+      description = "SSH Port Allow"
+      cidr_blocks = local.all_network
     }
+  ]
+  egress_with_cidr_blocks = [
+    {
+      from_port   = local.any_port
+      to_port     = local.any_port
+      protocol    = local.any_protocol
+      cidr_blocks = local.all_network
+    }
+  ]
+}
+
+
+# HTTP HTTPS SG
+module "HTTP_HTTPS_SG" {
+  source          = "github.com/Junghun-lovepower/Terraform_Project_SG"
+  name            = "HTTP_HTTPS_SG"
+  description     = "HTTP, HTTPS Port Allow"
+  vpc_id          = module.stage_vpc.vpc_id
+  use_name_prefix = "false"
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = local.http_port
+      to_port     = local.http_port
+      protocol    = local.tcp_protocol
+      description = "HTTP Port Allow"
+      cidr_blocks = local.all_network
+    },
+    {
+      from_port   = local.https_port
+      to_port     = local.https_port
+      protocol    = local.tcp_protocol
+      description = "HTTPS Port Allow"
+      cidr_blocks = local.all_network
+    }
+  ]
+
+  egress_with_cidr_blocks = [
+    {
+      from_port   = local.any_port
+      to_port     = local.any_port
+      protocol    = local.any_protocol
+      cidr_blocks = local.all_network
+    }
+  ]
+}
+
+
+# RDS SG
+module "RDS_SG" {
+  source          = "github.com/Junghun-lovepower/Terraform_Project_SG"
+  name            = "RDS_SG"
+  description     = "DB Port Allow"
+  vpc_id          = module.stage_vpc.vpc_id
+  use_name_prefix = "false"
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = local.db_port
+      to_port     = local.db_port
+      protocol    = local.tcp_protocol
+      description = "DB Port Allow"
+      cidr_blocks = local.private_subnets[0]
+    },
+    {
+      from_port   = local.db_port
+      to_port     = local.db_port
+      protocol    = local.tcp_protocol
+      description = "DB Port Allow"
+      cidr_blocks = local.private_subnets[1]
+    }
+  ]
+
+  egress_with_cidr_blocks = [
+    {
+      from_port   = local.any_port
+      to_port     = local.any_port
+      protocol    = local.any_protocol
+      cidr_blocks = local.all_network
+    }
+  ]
+}
+
+//==============================================//
+
+# BastionHost AWS KEY-Pair Data Source
+data "aws_key_pair" "EC2-Key" {
+  key_name = "EC2-key"
+}
+
+# BastionHost EIP
+resource "aws_eip" "BastionHost_eip" {
+  instance = aws_instance.BastionHost.id
+  tags = {
+    Name = "BastionHost_EIP"
   }
 }
 
-resource "aws_s3_bucket_versioning" "terraform_state_ver" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+# BastionHost Instance ( 
+resource "aws_instance" "BastionHost" {
+  ami                         = "ami-0ea4d4b8dc1e46212"
+  instance_type               = "t2.micro"
+  key_name                    = data.aws_key_pair.EC2-Key.key_name
+  subnet_id                   = module.stage_vpc.public_subnets[1]
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [module.SSH_SG.security_group_id]
 
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "myTerraform-bucket-lock-park-j"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
+  tags = {
+    Name = "BastionHost_Instance"
   }
-}
+} */
