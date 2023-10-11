6ee173e4a300432186565ddc268b2153c24faa21
Junghun-lovepower <tjdghks9585@gmail.com>
2023-10-11 14:20:38 +0900

    Remote Backend

---
+++ stage/VPC/variables.tf
@@ -0,0 +1,14 @@
+locals {
+  cidr             = "192.168.0.0/16"
+  azs              = ["ap-northeast-2a", "ap-northeast-2c"]
+  public_subnets   = ["10.10.1.0/24", "10.10.2.0/24"]
+  private_subnets  = ["10.10.10.0/24", "10.10.30.0/24"]
+  database_subnets = ["10.10.20.0/24", "10.10.40.0/24"]
+  ssh_port         = 22
+  http_port        = 80
+  https_port       = 443
+  any_port         = 0
+  any_protocol     = "-1"
+  tcp_protocol     = "tcp"
+  all_network      = "0.0.0.0/0"
+}
