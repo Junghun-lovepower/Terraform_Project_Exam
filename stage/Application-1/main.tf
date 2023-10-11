0efdef1fad191940e38edf372e64003409cd2b67
Junghun-lovepower <tjdghks9585@gmail.com>
2023-10-11 16:06:11 +0900

    Remote Backend

---
+++ stage/Application-1/main.tf
@@ -0,0 +1,22 @@
+
+terraform {
+  backend "s3" {
+    bucket  = "myterraform-bucket-state-park-j"
+    key     = "stage/app1/terraform.tfstate"
+    region  = "ap-northeast-2"
+    profile = "terraform_user"
+    dynamodb_table = "myTerraform-bucket-lock-park-j"
+    encrypt        = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "ap-northeast-2"
+  profile = "terraform_user"
+}
